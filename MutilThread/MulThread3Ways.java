e768c39e082b75e19566be5eb1063e93ea1a640f
pony <916837189@qq.com>
2020-06-21 20:48:10 +0800

    xiufu

--- MutilThread/MulThread3Ways.java
+++ MutilThread/MulThread3Ways.java
@@ -9,19 +9,26 @@ public class MulThread3Ways {
         }
     }
 
-    //实现
+    //实现Runnable接口的run方法
     public static class RunnableTask implements Runnable{
         @Override
         public void run(){
             System.out.println("I am a child thread");
         }
     }
+
+    public staic class CalllerTask implements  Callable<String>{
+        public String call() throws Exception{
+            return "hello";
+        }
+    }
     public static void  main(String []args){
 //        Mythread mythread = new Mythread();
 //        mythread.start();
-        RunnableTask runnableTask = new RunnableTask();
-        new Thread(runnableTask).start();
-        new Thread(runnableTask).start();
+//        RunnableTask runnableTask = new RunnableTask();
+//        new Thread(runnableTask).start();
+//        new Thread(runnableTask).start();
+
     }
 
 }
